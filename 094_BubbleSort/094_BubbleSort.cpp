60f9896dc2bbbf9405fc62eef20c7733b7b5f546
irjakniibos <irzayaumilsyahrar55@gmail.com>
2024-03-08 15:11:39 +0700

    deklarasi variabel global

--- 094_BubbleSort/094_BubbleSort.cpp
+++ 094_BubbleSort/094_BubbleSort.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; //deklarasi global array a dengan ukuran data 20
+int n;       //deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+
 
 int main()
 {
